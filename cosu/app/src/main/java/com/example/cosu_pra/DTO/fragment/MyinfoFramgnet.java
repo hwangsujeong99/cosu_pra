17a65275ea98706a41e532b200fb8bbb351146df
Seoyoon Heo <seoyoon130@kakao.com>
2021-05-16 17:50:30 +0900

    채팅방 ui 생성

--- cosu/app/src/main/java/com/example/cosu_pra/DTO/fragment/MyinfoFramgnet.java
+++ cosu/app/src/main/java/com/example/cosu_pra/DTO/fragment/MyinfoFramgnet.java
@@ -61,6 +61,6 @@ public class MyinfoFramgnet extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_myinfo_framgnet, container, false);
+        return inflater.inflate(R.layout.fragment_myinfo, container, false);
     }
 }
